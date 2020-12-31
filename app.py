7911ec4f2a986bc13672bc961c951684a8c36bb1
Jon Keam <jpkeam@gmail.com>
2020-12-30 21:29:21 -0500

    update message

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ topic_name = getenv('TOPIC_NAME', 'my-topic')
 times = int(getenv('TIMES', 1))
 
 uri = f'{bootstrap_server_host}:{bootstrap_server_port}'
+producer = KafkaProducer(bootstrap_servers=uri, acks=0, retries=5, batch_size=1)
+for i in range(times):
+    message = f'{{ "message": "Sending Message: {i}" }}'
+    producer.send(topic_name, message.encode())
 print(f'Kafka Bootstrap Server: {uri}')
 print(f'Topic: {topic_name}')
-producer = KafkaProducer(bootstrap_servers=uri)
-for i in range(times):
-    producer.send(topic_name, key=b'message', value=b'Sending Message')
 print(f'Sent {times} messages')
 producer.flush()
