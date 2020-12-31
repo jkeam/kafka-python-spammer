8da95ca382808d86c545890a260146f0454da44a
Jon Keam <jpkeam@gmail.com>
2020-12-30 23:25:00 -0500

    change ack strategy and flush after each to make it more synchronous

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ times = int(getenv('TIMES', 1))
 uri = f'{bootstrap_server_host}:{bootstrap_server_port}'
 
 try:
-    producer = KafkaProducer(bootstrap_servers=uri, acks=0, retries=5, batch_size=1)
+    producer = KafkaProducer(bootstrap_servers=uri, acks=1, retries=5, batch_size=1)
     for i in range(times):
         message = f'{{ "message": "Sending Message: {i}" }}'
         producer.send(topic_name, message.encode())
+        # purposely flush to make this sync instead of async
+        producer.flush()
     print(f'Kafka Bootstrap Server: {uri}')
     print(f'Topic: {topic_name}')
     print(f'Sent {times} messages')
 finally:
     if producer is not None:
-        producer.flush()
         producer.close()
